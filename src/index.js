ff9ce8d8b2fb30a7e1c80cd303ef731ad1c10197
translucent504 <fleshlime.1@gmail.com>
2020-06-10 18:05:19 +0500

    Wrote entire expense tracker with 1 component.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+
+
+const App = () => {
+  const [income, setIncome] = useState(0)
+  const [expense, setExpense] = useState(0)
+  const [incomehistory, setIncHistory] = useState([])
+  const [expensehistory, setExpHistory] = useState([])
+  const transname = React.createRef()
+  const transamount = React.createRef()
+
+  const handleSubmit = () => {
+    const transvalue = transamount.current.value
+    const tname = transname.current.value
+    const histitem = {name:tname, value:transvalue}
+    if(transvalue>0){
+      setIncome(income + parseInt(transvalue))
+      setIncHistory([...incomehistory, histitem])
+    }
+    if(transvalue<0){
+      setExpense(expense - parseInt(transvalue))
+      setExpHistory([...expensehistory, histitem])
+    }
+  }
+  
+  return (
+    <div>
+    <h1>Expense Tracker</h1>
+    <h2>Your Balance</h2>
+    ${income-expense}<br/>
+    Income: {income} Expense:{expense}
+    <h2>History</h2>
+    <ul>
+    {incomehistory.map((hist, index)=><li key={index}>{hist.name} +{hist.value}</li>)}
+    {expensehistory.map((hist, index)=><li key={index}>{hist.name} {hist.value}</li>)}
+    </ul>
+    <h2>Add New transaction</h2>
+    <input ref={transname} type="text" defaultValue="Transaction Name"/>
+    <input ref={transamount} type="number" defaultValue="Transaction Amount"/>
+    <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +53,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
