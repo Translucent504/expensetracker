063807cb126651fa23a2f04657107bacfbf9c394
translucent504 <fleshlime.1@gmail.com>
2020-06-11 21:01:29 +0500

    Added Github Repo Link

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <>
-            <h1>Expense Tracker</h1>
+            <h1><span><a href="https://github.com/Translucent504/expensetracker"><i className="fa fa-github" style={{fontSize:'96px'}}></i></a></span> Expense Tracker </h1> 
         </>
     )
 }
